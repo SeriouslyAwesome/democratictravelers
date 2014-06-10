132fd1e9ca5cf72e613da3e0474f86fbc9ac91e9
John Ellis <john@seriouslyawesome.com>
2014-06-10 14:14:54 -0500

    Don't serve static assets.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_assets = false
   config.static_cache_control = "public, max-age=31536000"
   config.action_controller.asset_host = ENV['CDN_SUMO_URL']
   config.assets.compress = true
