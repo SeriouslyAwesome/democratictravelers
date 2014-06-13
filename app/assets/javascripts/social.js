c220fd24888b8298e9a8be19ee980c155d0cabf7
John Ellis <john@seriouslyawesome.com>
2014-06-12 21:05:38 -0500

    More minor improvements to the blog.
    Restored Pinterest button, better caching.

--- app/assets/javascripts/social.js
+++ app/assets/javascripts/social.js
@@ -9,3 +9,13 @@
   js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=448393251865597&version=v2.0";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+
+// Pinterest
+(function(d){
+    var f = d.getElementsByTagName('SCRIPT')[0], p = d.createElement('SCRIPT');
+    p.type = 'text/javascript';
+    p.async = true;
+    p.setAttribute('data-pin-hover', true);
+    p.src = '//assets.pinterest.com/js/pinit.js';
+    f.parentNode.insertBefore(p, f);
+}(document));
