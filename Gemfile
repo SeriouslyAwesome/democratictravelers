c42ea4d2de6a04f814574c95e7e012de8834cb1c
John Ellis <john@seriouslyawesome.com>
2015-04-21 20:48:45 -0700

    Config Coveralls.

--- Gemfile
+++ Gemfile
@@ -103,4 +103,5 @@ group :development, :test do
   gem 'selenium-webdriver'
   gem 'shoulda-matchers', require: false
   gem 'simplecov', '~> 0.9.0', require: false
+  gem 'coveralls', require: false
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,7 @@
 require 'simplecov'
+require 'coveralls'
+
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
 SimpleCov.start
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
