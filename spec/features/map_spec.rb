4c0fb8691c4217bfa63d6e272e64ff83b30627ef
John Ellis <john@seriouslyawesome.com>
2015-06-02 20:53:37 -0600

    Fill gaps in map test coverage.

--- spec/features/map_spec.rb
+++ spec/features/map_spec.rb
@@ -10,4 +10,15 @@ feature 'The Map', js: true, vcr: true, record: :new_episodes do
   scenario 'shows suggestions in the sidebar' do
     expect(page).to have_content experience.name
   end
+
+  scenario 'can show a single experience' do
+    visit experience_path(experience)
+    expect(page).to have_content experience.name
+  end
+
+  scenario 'can show a single user' do
+    visit user_path(experience.user)
+    sleep 3
+    expect(page).to have_content experience.name
+  end
 end
